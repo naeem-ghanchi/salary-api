b6afc5ebdefcdbe0bdadf6d1a7bfda4854e0aa7e
naeem <gnaeem111@gmail.com>
2026-04-15 20:29:41 +0530

    refactor: remove employee routes from app.js and use route file

--- src/app.js
+++ src/app.js
@@ -1,23 +1,9 @@
 const express = require("express");
-const {
-  createEmployee,
-  getAllEmployees,
-  getEmployeeById,
-  updateEmployee,
-  deleteEmployee,
-} = require("./controllers/employee.controller");
 const app = express();
+const employeeRoutes = require("./routes/employee.routes");
 
 app.use(express.json());
 
-app.post("/employee", createEmployee);
-
-app.get("/employee", getAllEmployees);
-
-app.get("/employee/:id", getEmployeeById);
-
-app.put("/employee/:id", updateEmployee);
-
-app.delete("/employee/:id", deleteEmployee);
+app.use("/employee", employeeRoutes);
 
 module.exports = app;
--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const controller = require("../controllers/employee.controller");
 
-router.post("/employee", controller.createEmployee);
-router.get("/employee", controller.getAllEmployees);
-router.get("/employee/:id", controller.getEmployeeById);
-router.put("/employee/:id", controller.updateEmployee);
-router.delete("/employee/:id", controller.deleteEmployee);
+router.post("/", controller.createEmployee);
+router.get("/", controller.getAllEmployees);
+router.get("/:id", controller.getEmployeeById);
+router.put("/:id", controller.updateEmployee);
+router.delete("/:id", controller.deleteEmployee);
 
 module.exports = router;
