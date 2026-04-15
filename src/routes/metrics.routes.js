a979765cb1d16ff4e82a1060738c94fa2f472efc
naeem <gnaeem111@gmail.com>
2026-04-15 21:43:55 +0530

    feat: add job metrics route

--- src/routes/metrics.routes.js
+++ src/routes/metrics.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/metrics.controller");
 
 router.get("/country/:country", controller.getCountryMetrics);
+router.get("/job/:jobTitle", controller.getJobMetrics);
 
 module.exports = router;
