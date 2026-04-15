2b76e62c538613ae0a37c7feb1ac99ed41ed8451
naeem <gnaeem111@gmail.com>
2026-04-15 20:35:35 +0530

    feat: implement salary calculation logic

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -11,3 +11,22 @@ exports.getById = (id) => repo.findById(id);
 exports.updateEmployee = (id, data) => repo.update(id, data);
 
 exports.deleteEmployee = (id) => repo.delete(id);
+
+exports.calculateSalary = async (id) => {
+  const emp = await repo.findById(id);
+  if (!emp) return null;
+
+  let deduction = 0;
+
+  if (emp.country === "India") {
+    deduction = emp.salary * 0.1;
+  } else if (emp.country === "United States") {
+    deduction = emp.salary * 0.12;
+  }
+
+  return {
+    gross: emp.salary,
+    deduction,
+    net: emp.salary - deduction,
+  };
+};
